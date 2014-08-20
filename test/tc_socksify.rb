e3560734a049ce0937bc0230b670651c28f5666a
Vasily Shmelev <sleephunter@gmail.com>
2014-08-20 19:15:42 +0400

    Fix test suite, remove all monkeypatches

--- test/tc_socksify.rb
+++ test/tc_socksify.rb
@@ -8,18 +8,10 @@ $:.unshift "#{File::dirname($0)}/../lib/"
 require 'socksify'
 require 'socksify/http'
 
-# XXX: Monkey patches
-module URI
-  class Generic
-    def empty?
-      self.to_s.empty?
-    end
-  end
-end
-
 class SocksifyTest < Test::Unit::TestCase
   def setup
     Socksify::debug = true
+    my_ip
   end
 
   def disable_socks
@@ -28,11 +20,11 @@ class SocksifyTest < Test::Unit::TestCase
   end
   def enable_socks
     TCPSocket.socks_server = "127.0.0.1"
-    TCPSocket.socks_port = 9150
+    TCPSocket.socks_port = 9050
   end
 
   def http_tor_proxy
-    Net::HTTP::SOCKSProxy("127.0.0.1", 9150)
+    Net::HTTP::SOCKSProxy("127.0.0.1", 9050)
   end
 
   def test_check_tor
@@ -55,8 +47,10 @@ class SocksifyTest < Test::Unit::TestCase
   def test_check_tor_via_net_http
     disable_socks
 
-    [['Hostname', :check_tor],
-     ['IPv4', :check_tor_ip]].each do |f_name, f|
+     [
+        ['IPv4', :check_tor_ip],
+        ['Hostname', :check_tor]
+     ].each do |f_name, f|
       tor_direct, ip_direct = send(f)
       assert_equal(false, tor_direct)
 
@@ -74,7 +68,7 @@ class SocksifyTest < Test::Unit::TestCase
     assert_equal(false, tor_direct)
 
     enable_socks
-    TCPSocket.socks_ignores << 'check.torproject.org'
+    TCPSocket.socks_ignores << 'internet.yandex.com'
 
     tor_socks_ignored, ip_socks_ignored = check_tor
     assert_equal(false, tor_socks_ignored)
@@ -85,39 +79,53 @@ class SocksifyTest < Test::Unit::TestCase
   def get_http(http_klass, url)
     uri = URI(url)
     body = nil
+
+
+
+    req = Net::HTTP::Get.new(uri.request_uri)
+    req['Host'] = "internet.yandex.com"
+    req['User-Agent'] = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_4) AppleWebKit/536.5 (KHTML, like Gecko) Chrome/19.0.1084.46 Safari/536.5"
+
     http_klass.start(uri.host, uri.port,
                      :use_ssl => uri.scheme == 'https',
                      :verify_mode => OpenSSL::SSL::VERIFY_NONE) do |http|
-      req = Net::HTTP::Get.new uri
-      req['Host'] = "check.torproject.org"
-      req['User-Agent'] = "ruby-socksify test"
+
       body = http.request(req).body
     end
+
     body
   end
 
   def check_tor(http_klass = Net::HTTP)
-    parse_check_response get_http(http_klass, 'https://check.torproject.org/')
+    parse_check_response get_http(http_klass, 'https://internet.yandex.com')
   end
 
   def check_tor_ip(http_klass = Net::HTTP)
-    parse_check_response get_http(http_klass, 'https://38.229.72.22/')  # "check.torproject.org"
+    parse_check_response get_http(http_klass, 'https://213.180.204.248')  # "internet.yandex.com"
+  end
+
+  def my_ip
+    disable_socks
+
+    body = get_http(Net::HTTP, 'http://internet.yandex.com')
+
+    if body =~ /My IPv4\: (\d+\.\d+\.\d+\.\d+)/
+      @ip = $1
+    else
+      @ip = nil
+    end
+
   end
 
   def parse_check_response(body)
-    if body.include? 'This browser is configured to use Tor.'
-      is_tor = true
-    elsif body.include? 'You are not using Tor.'
-      is_tor = false
-    else
-      raise "Bogus response #{body}"
-    end
-
-    if body =~ /Your IP address appears to be:\s*<strong>(\d+\.\d+\.\d+\.\d+)<\/strong>/
+    if body =~ /My IPv4: (\d+\.\d+\.\d+\.\d+)/
       ip = $1
     else
       raise 'Bogus response, no IP'
     end
+
+    is_tor = !(@ip == ip)
+
     [is_tor, ip]
   end
 
